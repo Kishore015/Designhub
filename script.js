54e2f2d2300df62814cb2d9b3766b94a3c452f76
Kishore-95 <mvs.kishore95@gmail.com>
2022-03-18 12:41:02 +0530

    design hub template created

---
+++ script.js
@@ -0,0 +1,13 @@
+// show slot timings
+
+const date_id = document.getElementById('date');
+
+
+
+
+date_id.addEventListener('input', ()=>{
+    document.getElementById('show').classList = "d-block";
+})
+
+
+console.log(date_id);
