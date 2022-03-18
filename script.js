9eb18b36b8c442bcd6f24f84254cd7fe6ba9adf6
Kishore-95 <mvs.kishore95@gmail.com>
2022-03-18 13:12:50 +0530

    changes updated in form

--- script.js
+++ script.js
@@ -2,7 +2,15 @@
 
 const date_id = document.getElementById('date');
 
+const form  = document.getElementById('form');
 
+form.addEventListener('submit', ()=>{
+   let name = document.getElementById('name');
+   let email =  document.getElementById('email');
+   let phone = document.getElementById('email');
+   let date_id = this.date_id;
+   
+})
 
 
 date_id.addEventListener('input', ()=>{
