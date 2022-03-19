33fb10b7f848c4e99ecedd8c6992b24ceff02a79
Kishore-95 <mvs.kishore95@gmail.com>
2022-03-19 15:59:14 +0530

    created footer

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
-// show slot timings
 
-const date_id = document.getElementById('date');
 
-const form  = document.getElementById('form');
+let js_date = new Date();
+
+let today = js_date.getDate();
+
+let month = js_date.getMonth() + 1;
+
+if(today < 10){
+    today = "0" + today;
+}
+
+if(month<10){
+    month = "0" + month;
+}
+
+let year = js_date.getFullYear();
+
+let currentDate =  year + "-" + month + "-" + today;
+
+let date1 = document.getElementById('date').setAttribute('min', currentDate);
+
+console.log(date1);
+//console.log(currentDate);
+
+let form  = document.getElementById('form');
 
 form.addEventListener('submit', ()=>{
    let name = document.getElementById('name');
    let email =  document.getElementById('email');
-   let phone = document.getElementById('email');
-   let date_id = this.date_id;
+   let phone = document.getElementById('phone');
    
 })
 
-
-date_id.addEventListener('input', ()=>{
+// show slot timings
+date.addEventListener('input', ()=>{
     document.getElementById('show').classList = "d-block";
 })
 
-
-console.log(date_id);
